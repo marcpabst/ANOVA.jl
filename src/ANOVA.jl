18b689de78f6390a8c5bb1db6bd910affe64d0de
mxtra <mail@marcpabst.de>
2018-06-26 00:46:31 +0200

    experimental stuff

--- src/ANOVA.jl
+++ src/ANOVA.jl
@@ -1,42 +1,69 @@
 module ANOVA
 
 using GLM, DataFrames, Distributions, CategoricalArrays, ArgCheck
+import StatsBase
+
+struct AnovaObject
+  Source::Array{String,1}
+  DF::Array{AbstractFloat,1}
+  SS::Array{AbstractFloat,1}
+  MSS::Array{AbstractFloat,1}
+  F::Array{AbstractFloat,1}
+  p::Array{AbstractFloat,1}
+end
 
 
-## helper functions
+struct AnovaDataFrameRegressionModel{M,T} <: StatsModels.RegressionModel
+  model::M
+  mf::ModelFrame
+  mm
+  anova::AnovaObject
+end
+
+struct AnovaOptions
+  anovatype::Int
+end
+
+
+function StatsBase.fit(::Type{LinearModel}, f::Formula, df::AbstractDataFrame, options::AnovaOptions,args...; contrasts::Dict = Dict(), kwargs...)
+
+  @argcheck options.anovatype == 1 || options.anovatype == 3 "'anovatype' in 'options' must be either '1' or '3'"
+
+  trms = StatsModels.Terms(f)
+  StatsModels.drop_intercept(LinearModel) && (trms.intercept = true)
+  mf = ModelFrame(trms, df, contrasts=contrasts)
+  StatsModels.drop_intercept(LinearModel) && (mf.terms.intercept = false)
+  mm = ModelMatrix(mf)
+  y = StatsModels.model_response(mf)
+
+
+  model = fit(LinearModel, mm.m, y, args...; kwargs...)
+  AnovaDataFrameRegressionModel{LinearModel,Any}(model, mf, mm, computeanova(model, mf, mm; options.anovatype))
+end
+
+## helper functions 
+
+# calculate effects for type I ANOVA (I stole this somewhere, but don't remember where :( )
 function effects(mod)
     return (mod.pp.X / cholfact!(mod.pp)[:U])' * mod.rr.y
   end
 
+# this will drop variables from the matrix using the mask argument and fit a new linear model
 function droptermbymask(mod, mask)
-    matrix = mod.model.pp.X[:,.!mask]
-    y = mod.model.rr.y
+    matrix = mod.pp.X[:,.!mask]
+    y = mod.rr.y
     return lm(matrix,y)
 end
 
-"""
-    anova(model[,anovatype=3,siglevel=0.05])
+# calculate ANOVA
+function computeanova(mod::LinearModel, mf::ModelFrame, mm::ModelMatrix; anovatype)
 
-Calculate an ANOVA from the given linear model.
+  eff = effects(mod) # get effect sizes 
 
-!!! warning "Do not use type I ANOVA with unbalanced data!"
-
-    And this is another one.
-
-    This warning admonition has a custom title: `"Beware!"`.
-"""
-function anova(mod; anovatype = 3, siglevel=0.05)
-
-  @argcheck anovatype == 1 || anovatype == 3 "Argument 'anovatype' must be either '1' or '3'"
-
-  if(anovatype==1) warn("Type I ANOVAs should used with care (or not at all) if your data is unbalanced.") end
-
-  eff = effects(mod.model) # get effect sizes 
-
-  T = eltype(mod.model.pp.X)
-  response = mod.mf.terms.eterms[1]
-  terms = mod.mf.terms.terms
-  assign = mod.mm.assign
+  T = eltype(mod.pp.X)
+  response = mf.terms.eterms[1]
+  terms = mf.terms.terms
+  assign = mm.assign
 
   ## calculate variables for residuals (this is the full / saturated modell) ##
   DFres = dof_residual(mod)           # degrees of freedom
@@ -45,7 +72,7 @@ function anova(mod; anovatype = 3, siglevel=0.05)
   MSSres = SSres / DFres              # calculate mean sum of squares for residuals
 
   # check if there is an intercept
-  k = all(mod.model.pp.X[:,1] .== 1.0) ? 1 : 0
+  k = all(mod.pp.X[:,1] .== 1.0) ? 1 : 0
 
   # get assigned numbers for factors
   factors_assig = unique(assign)
@@ -61,8 +88,7 @@ function anova(mod; anovatype = 3, siglevel=0.05)
   MSS = zeros(T,Nfactors)  # will contain mean sum of squares for factor
   RSS = zeros(T,Nfactors)  # 
   F = zeros(T,Nfactors)    # will contain F values for factors
-  pval = zeros(T,Nfactors) # will contain pvalue for factors
-  pvals = Array{String}(Nfactors) # will contain pvalue for factors
+  p = zeros(T,Nfactors)    # will contain pvalue for factors
 
 
 for i in 1:Nfactors
@@ -74,7 +100,7 @@ for i in 1:Nfactors
     SS[i] = RSS[i] - deviance(mod)
     DF[i] = sum(mask)
     MSS[i] = SS[i]/DF[i]
-    F[i] = ftest(mod.model,newmod).fstat[1]
+    F[i] = ftest(mod,newmod).fstat[1]
   else
     SS[i] = sum(abs2.(eff[mask]))
     RSS[i] = 0
@@ -83,21 +109,36 @@ for i in 1:Nfactors
     F[i] = MSS[i]/MSSres
   end
 
-  pval[i] = ccdf(FDist(DF[i], DFres), F[i])
-  pvals[i] = if pval[i] < siglevel string(pval[i],"*") else string(pval[i]) end
+  p[i] = ccdf(FDist(DF[i], DFres), F[i])
+  #pvals[i] = if pval[i] < siglevel string(pval[i],"*") else string(pval[i]) end
 end
-println("ANOVA table for linear model")
 
-DataFrame( 
-          Source = vcat(terms,"Residuals"),
-          DF = vcat(DF,DFres),
-          SS = vcat(SS, SSres),
-          MSS = vcat(MSS, MSSres),
-          F = vcat(F, ""),
-          p = vcat(pvals, "")
+AnovaObject( 
+          vcat(string.(terms),"Residuals"),
+          vcat(DF,DFres),
+          vcat(SS, SSres),
+          vcat(MSS, MSSres),
+          vcat(F, -1),
+          vcat(p, -1)
           )
 end
 
-export anova
+StatsBase.predict(m::AnovaDataFrameRegressionModel) = predict(m.model)
+StatsBase.residuals(m::AnovaDataFrameRegressionModel) = residuals(m.model)
+
+function Base.show(io::IO, m::AnovaDataFrameRegressionModel)
+  x = m.anova
+  println("ANOVA table for linear model")
+  print(DataFrame( 
+          Source = x.Source,
+          DF = x.DF,
+          SS = x.SS,
+          MSS = x.MSS,
+          F = x.F,
+          p = x.p
+          ))
+end
+
+export AnovaDataFrameRegressionModel, AnovaOptions
 
 end # module
