0a23ea2bb00e3ca1e94b5bab180d04beb7bbff5d
mxtra <mail@marcpabst.de>
2018-06-26 15:40:23 +0200

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,6 @@ end
 
 # # write your own tests here
 
-
 data = dataset("datasets", "ToothGrowth")
 data[:Dose] = categorical(data[:Dose])
 
@@ -16,11 +15,6 @@ model = fit(LinearModel,
             @formula(Len ~  Supp + Dose), 
             data, 
             contrasts = Dict(:Supp => EffectsCoding(),:Dose => EffectsCoding()))
-
-            (Intercept) 2326.91  1 158.828 < 2.2e-16 ***
-supp         205.35  1  14.017 0.0004293 ***
-dose        2426.43  2  82.811 < 2.2e-16 ***
-Residuals    820.43 56  
 
 @test ANOVA.anova(model).p[1] ≈ 0.00042927927678531035
 @test ANOVA.anova(model).p[2] ≈ 1.8711626362930449e-17
